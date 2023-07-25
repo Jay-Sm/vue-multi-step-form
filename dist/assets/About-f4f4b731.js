f9fd0a8f702b32101969cd95206b8491f654c518
Jay Sm <jaysmailg14@gmail.com>
2023-07-25 14:55:48 -0400

    Added dist, added more CSS + Postponing project bc its boring

---
+++ dist/assets/About-f4f4b731.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-8f73dc5c.js";const o={},s={class:"text-4xl p-5"};function n(r,_){return t(),c("h1",s,"About View")}const f=e(o,[["render",n]]);export{f as default};
--- src/views/PersonalInfo.vue
+++ src/views/PersonalInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white h-4/6 w-8/12 min-w-[700px] min-h-[400px]">
+  <div class="bg-white h-4/6 w-8/12 min-w-[700px] min-h-[400px] rounded-lg">
 
   </div>
 </template>
